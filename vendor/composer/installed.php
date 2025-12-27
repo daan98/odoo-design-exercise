2ba3d474c77273908aef7cef84ddb3e7d0d6d88f
daan98 <daan98.da@gmail.com>
2025-12-27 01:51:37 -0600

    Bootstrap is working:
    - Bootstrap classes works.
    - Icons work.
    - Styles for navbar included.
    - Responsive navbar included (need fixed).

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a9dbd543f871fb9a04490daaadb0260302ed3b8d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a9dbd543f871fb9a04490daaadb0260302ed3b8d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twbs/bootstrap-icons' => array(
+            'pretty_version' => 'v1.13.1',
+            'version' => '1.13.1.0',
+            'reference' => 'ce0e49dd063243118a115f17ad1fe1fe7576d552',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twbs/bootstrap-icons',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
